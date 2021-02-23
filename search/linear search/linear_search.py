52b1dbf875f8ef072c7b5f64a661ddd09022fdfb
likweitan <likweitan@gmail.com>
2021-02-23 12:42:09 +0800

    First commit

---
+++ search/linear search/linear_search.py
@@ -0,0 +1,11 @@
+# Problem: Given an array arr[] of n elements, write a function to search a given element x in arr[].
+
+arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+x = 110
+
+for i in arr:
+    if x == i:
+        break
+
+
+print('Element x is present at index 6')
